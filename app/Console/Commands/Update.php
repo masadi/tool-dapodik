470f7633c9927a441b41887c5ef3cbdbdda46747
masadi <masadi.com@gmail.com>
2025-08-02 20:47:17 +0700

    port

---
+++ app/Console/Commands/Update.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Update extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Silahkan tunggu, sedang proses update aplikasi....');
+        //exec("git pull origin main");
+        exec("git pull https://github.com/masadi/tool-dapodik.git");
+        exec("git clean -df");
+        exec("git stash");
+        exec("git config --global --add safe.directory C:/tool-dapodik/dataweb");
+        exec("composer update");
+    }
+}
