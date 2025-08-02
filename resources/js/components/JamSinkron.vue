d20fc5c6e52c552656744bad9e8a38206dae1816
masadi <masadi.com@gmail.com>
2025-08-03 01:53:30 +0700

    TARIK DATA

--- resources/js/components/JamSinkron.vue
+++ resources/js/components/JamSinkron.vue
@@ -1,9 +1,9 @@
 <template>
   <VAlert color="error" class="text-center" variant="tonal">
-    <h2 class="mt-4 mb-4">Penyesuaian Waktu Layanan Tarik Data Dapodik</h2>
+    <h2 class="mt-4 mb-4">Penyesuaian Waktu Layanan TARIK DATA Dapodik</h2>
     <p>
       Dikarenakan adanya proses rutin sinkronisasi data Dapodik di Server,
-      <br />maka layanan Tarik Data hanya dapat diakses antara pukul
+      <br />maka layanan TARIK DATA hanya dapat diakses antara pukul
       <span class="text-danger"><b>03.00 s/d 00.00 (WIB)</b></span>
     </p>
   </VAlert>
